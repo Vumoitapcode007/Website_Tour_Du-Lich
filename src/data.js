b8e7711b66ca76e1236f360c9755e648521ac0a8
Hà Quốc Việt <soibeti2k5@gmail.com>
2026-09-25 13:11:29 +0700

    thêm sản phẩm

--- src/data.js
+++ src/data.js
@@ -17,7 +17,7 @@ export const tours = [
     price: 2490000,
     time: "3 ngày 2 đêm",
     image:
-      "https://images.unsplash.com/photo-1559592413-7cec4d0cae0b?auto=format&fit=crop&w=800&q=80",
+      "https://images.unsplash.com/photo-1674296067534-0f9769040781?auto=format&fit=crop&w=800&q=80",
     description: "Trải nghiệm biển Đà Nẵng và phố cổ Hội An.",
   },
   {
@@ -37,8 +37,28 @@ export const tours = [
     price: 2190000,
     time: "3 ngày 2 đêm",
     image:
-      "https://images.unsplash.com/photo-1559592413-7cec4d0cae0b?auto=format&fit=crop&w=800&q=80",
+      "https://images.unsplash.com/photo-1750021875875-c6151cf78269?auto=format&fit=crop&w=800&q=80",
     description: "Khám phá thành phố ngàn hoa.",
+  },
+  {
+    id: 5,
+    name: "Tour Sa Pa",
+    location: "Sa Pa",
+    price: 2890000,
+    time: "3 ngày 2 đêm",
+    image:
+      "https://images.unsplash.com/photo-1666160416071-f760a7af9ea6?auto=format&fit=crop&w=800&q=80",
+    description: "Khám phá ruộng bậc thang và núi rừng Tây Bắc.",
+  },
+  {
+    id: 6,
+    name: "Tour Phú Quốc",
+    location: "Phú Quốc",
+    price: 3290000,
+    time: "3 ngày 2 đêm",
+    image:
+      "https://images.unsplash.com/photo-1732243395944-cb3ff9311091?auto=format&fit=crop&w=800&q=80",
+    description: "Nghỉ dưỡng bờ biển và ngắm hoàng hôn tại đảo ngọc.",
   },
 ];
 
